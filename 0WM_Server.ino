253664b885560820fc8c578aa3782a64d7c3ad21
Ullas M S <33162102+uniqueullas@users.noreply.github.com>
2020-06-06 23:26:55 +0530

    Update 0WM_Server.ino

--- 0WM_Server.ino
+++ 0WM_Server.ino
@@ -13,11 +13,7 @@ BMP180I2C bmp180(I2C_ADDRESS);
 #include <Arduino.h>
 DHT dht(DHTPIN, DHTTYPE);
 
-#include "Sodaq_DS3231.h"
 #include <Wire.h>
-#define DISPLAY_DELAY 1000 // oled display delay 1s
-#include "SSD1306Wire.h"
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 char auth[] = "6UqgH13ON98rLmxl3b2mO89xjJUoIIG1";
 
@@ -26,7 +22,6 @@ char pass[] = "12345678";
 
 
 int status;
-
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);    // The begin call takes the width and height. This
@@ -51,60 +46,66 @@ void setup() {
   bmp180.setSamplingMode(BMP180MI::MODE_UHR);
 }
 
+int t1;
+int t2;
+int t3;
+int h1;
+int h2;
+int h3;
+int p1;
+
 void loop() {
-  {
-    if (Serial.available() > 0) {
-      String input;
-      input = Serial.readStringUntil('\n');
-      if (input[0] == 'c') {
-        WiFi.begin(ssid, pass);
-        Serial.println("connecting...");
-      }
-      else if (input[0] == 'd') {
-        WiFi.disconnect();
-        Serial.println("disconnecting...");
-      }
+  sp();
+  lp();
+  if (Serial.available() > 0) {
+    String input;
+    input = Serial.readStringUntil('\n');
+    if (input[0] == 'c') {
+      WiFi.begin(ssid, pass);
+      Serial.println("connecting...");
     }
-    if (WiFi.status() == WL_IDLE_STATUS) { // when not connected to a network, but powered on
-      // lcd.setCursor(-4, 3);
-      // lcd.print("WL_IDLE_STATUS");
-      Serial.println("WL_IDLE_STATUS");
+    else if (input[0] == 'd') {
+      WiFi.disconnect();
+      Serial.println("disconnecting...");
     }
-    else if (WiFi.status() == WL_CONNECTED) {
-      //  lcd.setCursor(-4, 3);
-      //  lcd.print(" Wifi_CONNECTED ");
-      Serial.print("WL_CONNECTED ");
-      Serial.println(WiFi.localIP());
-    }
-    else if (WiFi.status() == WL_NO_SSID_AVAIL) {
-      // lcd.setCursor(-4, 3);
-      // lcd.print("WL_NO_SSID_AVAIL");
-      Serial.println("WL_NO_SSID_AVAIL");
-    }
-    else if (WiFi.status() == WL_CONNECT_FAILED) {
-      // lcd.setCursor(-4, 3);
-      // lcd.print("WL_CONNECT_FAILED");
-      Serial.println("WL_CONNECT_FAILED");
-    }
-    else if (WiFi.status() == WL_CONNECTION_LOST) {
-      // lcd.setCursor(-4, 3);
-      // lcd.print("CONNECTION--LOST");
-      Serial.println("WL_CONNECTION_LOST");
-    }
-    else if (WiFi.status() == WL_DISCONNECTED) {
-      // lcd.setCursor(-4, 3);
-      // lcd.print("Wfi_DISCONNECTED");
-      Serial.println("WL_DISCONNECTED");
-    }
-    else {
-      digitalWrite(BUILTIN_LED, HIGH);
-      Serial.print("unknown status: ");
-      Serial.println(WiFi.status());
-    }
-    delay(200);
   }
-  Blynk.run();
-  BLYNK_WRITE();
+  if (WiFi.status() == WL_IDLE_STATUS) { // when not connected to a network, but powered on
+    // lcd.setCursor(-4, 3);
+    // lcd.print("WL_IDLE_STATUS");
+    Serial.println("WL_IDLE_STATUS");
+  }
+  else if (WiFi.status() == WL_CONNECTED) {
+    //  lcd.setCursor(-4, 3);
+    //  lcd.print(" Wifi_CONNECTED ");
+    Serial.print("WL_CONNECTED ");
+    Serial.println(WiFi.localIP());
+  }
+  else if (WiFi.status() == WL_NO_SSID_AVAIL) {
+    // lcd.setCursor(-4, 3);
+    // lcd.print("WL_NO_SSID_AVAIL");
+    Serial.println("WL_NO_SSID_AVAIL");
+  }
+  else if (WiFi.status() == WL_CONNECT_FAILED) {
+    // lcd.setCursor(-4, 3);
+    // lcd.print("WL_CONNECT_FAILED");
+    Serial.println("WL_CONNECT_FAILED");
+  }
+  else if (WiFi.status() == WL_CONNECTION_LOST) {
+    // lcd.setCursor(-4, 3);
+    // lcd.print("CONNECTION--LOST");
+    Serial.println("WL_CONNECTION_LOST");
+  }
+  else if (WiFi.status() == WL_DISCONNECTED) {
+    // lcd.setCursor(-4, 3);
+    // lcd.print("Wfi_DISCONNECTED");
+    Serial.println("WL_DISCONNECTED");
+  }
+  else {
+    digitalWrite(BUILTIN_LED, HIGH);
+    Serial.print("unknown status: ");
+    Serial.println(WiFi.status());
+  }
+
 
   if (!bmp180.measureTemperature())
   {
@@ -118,20 +119,9 @@ void loop() {
     delay(100);
   } while (!bmp180.hasValue());
 
-  Serial.print("Temperature: ");
-  Serial.print(bmp180.getTemperature());
-  Serial.println(" degC");
-  Blynk.virtualWrite(0, bmp180.getTemperature());
+  t1 = bmp180.getTemperature();
+  Blynk.virtualWrite(0, t1);
 
-  lcd.setCursor(0, 0); // Move the cursor characters to the right and
-  lcd.print("T1 ");  // Print HELLO to the screen, starting at 0,0.
-  lcd.print(bmp180.getTemperature());
-
- // Blynk.notify("NODE MCU BOARD1 is ON AIR...!");
- // blynk.email("ullas6558@gmail.com", "PROJECT 2020", "HI test code is UP");
-
-  //start a pressure measurement. pressure measurements depend on temperature measurement, you should only start a pressure
-  //measurement immediately after a temperature measurement.
   if (!bmp180.measurePressure())
   {
     Serial.println("could not start perssure measurement, is a measurement already running?");
@@ -142,61 +132,87 @@ void loop() {
     delay(100);
   } while (!bmp180.hasValue());
 
-  Serial.print("Pressure: ");
-  Serial.print(bmp180.getPressure());
-  Serial.println(" Pa");
-  Blynk.virtualWrite(6, bmp180.getPressure());
+  p1 = bmp180.getPressure();
+  Blynk.virtualWrite(6, p1);
 
-  lcd.setCursor(0, 1); // Move the cursor characters to the right and
-  lcd.print("P1 ");  // Print HELLO to the screen, starting at 0,0.
-  lcd.print(bmp180.getPressure());
-  //lcd.print("pa");
+  h1 = dht.readHumidity();
+  Blynk.virtualWrite(1, h1);
 
-  float uh = dht.readHumidity();
-  Serial.print("humidity: ");
-  Serial.println(uh);
-  Blynk.virtualWrite(1, dht.readHumidity());
-
-  lcd.setCursor(8, 0); // Move the cursor characters to the right and
-  lcd.print("H1:");  // Print HELLO to the screen, starting at 0,0.
-  lcd.print(dht.readHumidity());
-  //lcd.print("%");
-  delay(5000);
+  Blynk.run();
+  BLYNK_WRITE();
 }
 BLYNK_WRITE(V2) {
-  float pinData2 = param.asFloat();
-  Serial.println(String("temp2: ") + pinData2);
-  lcd.setCursor(0, 0); // Move the cursor characters to the right and
-  //lcd.clear();
-  delay(1000);
-  lcd.print("T2 ");  // Print HELLO to the screen, starting at 0,0.
-  lcd.print(pinData2);
-  //lcd.print("C");
+  t2 = param.asFloat();
+  // Serial.print(String("testing under loop t2: ") + t2);
+  Serial.println(" degC");
 }
 BLYNK_WRITE(V3) {
-  float pinData3 = param.asFloat();
-  Serial.println(String("hum: ") + pinData3);
-  lcd.setCursor(0, 1); // Move the cursor characters to the right and
-  lcd.print("H2 ");  // Print HELLO to the screen, starting at 0,0.
-  lcd.print(pinData3);
-  delay(5000);
-  //lcd.print("%");
+  h2 = param.asFloat();
 }
-/BLYNK_WRITE(V4) {
-  float pinData4 = param.asFloat();
-  Serial.println(String("temp3: ") + pinData4);
-  lcd.setCursor(8, 0); // Move the cursor characters to the right and
-  lcd.print("T3:");  // Print HELLO to the screen, starting at 0,0.
-  lcd.print(pinData4);
-  lcd.print((char)223);
-  lcd.print("C");
-  }
-  BLYNK_WRITE(V5) {
-  float pinData5 = param.asFloat();
-  Serial.println(String("hum: ") + pinData5);
-  lcd.setCursor(8, 1); // Move the cursor characters to the right and
-  lcd.print("H3:");  // Print HELLO to the screen, starting at 0,0.
-  lcd.print(pinData5);
-  //lcd.print("%");
-  delay(5000);
-  }
+BLYNK_WRITE(V4) {
+  t3 = param.asFloat();
+}
+BLYNK_WRITE(V5) {
+  h3 = param.asFloat();
+}
+
+void sp() {
+  Serial.print(String("t1: ") + t1);
+  Serial.println(" degC");
+  Serial.print(String("p1: ") + p1);
+  Serial.println(" Pa");
+  Serial.print(String("h1: ") + h1);
+  Serial.println(" %");
+  Serial.print(String("t2: ") + t2);
+  Serial.println(" degC");
+  Serial.print(String("h2: ") + h2);
+  Serial.println(" %");
+  Serial.print(String("t3: ") + t3);
+  Serial.println(" degC");
+  Serial.print(String("h3: ") + h3);
+  Serial.println(" %");
+}
+
+void lp() {
+  lcd.clear();
+
+  lcd.setCursor(4, 0);
+  lcd.print("T1:");
+  lcd.print(t1);
+  lcd.print("*c");
+
+  lcd.setCursor(0, 1);
+  lcd.print("H1:");
+  lcd.print(h1);
+  lcd.print("%");
+
+  lcd.setCursor(10, 1);
+  lcd.print("P1:");
+  lcd.print(p1 % 100);
+  lcd.print("pa");
+
+  delay (3000);
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("T2:");
+  lcd.print(t2);
+  lcd.print("*c");
+
+  lcd.setCursor(0, 1);
+  lcd.print("H2:");
+  lcd.print(h2);
+  lcd.print("%");
+
+  lcd.setCursor(10, 0);
+  lcd.print("T3:");
+  lcd.print(t3);
+  lcd.print("*c");
+
+  lcd.setCursor(10, 1);
+  lcd.print("H3:");
+  lcd.print(h3);
+  lcd.print("%");
+
+  delay (3000);
+}
